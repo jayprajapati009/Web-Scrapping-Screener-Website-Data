5821f2e97723a4e57f5bb33ccdc567fb25c9f225
jayprajapati009 <jayprajapati9900@gmail.com>
2022-06-14 10:49:14 +0530

    Test Code Uplod for fetching dates

--- test_code.py
+++ test_code.py
@@ -2,35 +2,24 @@ from bs4 import BeautifulSoup as bs
 from numpy import append
 import requests as re
 import os
-from selenium.webdriver import chrome
-from selenium.webdriver import ChromeOptions
 
 os.chdir(r'C:\Users\jaypr\Desktop\Tech Stack\VSCodes\Web Scrapping\StockScrapping')
 
 
 def find_data():
 
-    option = ChromeOptions()
-    option.headless = True
-    driver = chrome(
-        executable_path=r'C:\Users\jaypr\Downloads\chromedriver_win32/chromedriver.exe')
-    driver.get(
-        'https://www.screener.in/company/540416/consolidated/#balance-sheet')
-
     html_text = re.get(
         'https://www.screener.in/company/540416/consolidated/#balance-sheet').text
     # html_text = re.get(
     #     'https://www.screener.in/company/540455/').text
 
-    soup = bs(driver, 'lxml')
+    soup = bs(html_text, 'lxml')
 
     cashEq = soup.find(
-        'section', id='balance-sheet', class_='card card-large').children
-    print(cashEq)
-    # val_i = cashEq.find('Borrowings')
-    # othli_i = cashEq.find('Other Liabilities')
-    # cashEq_list = cashEq[val_i + 10: othli_i].strip().split('\n')
-    driver.quit()
+        'section', id='balance-sheet', class_='card card-large').table.thead.tr.text
+    cashEq_list = cashEq.strip().split('/n')
+
+    print(cashEq_list)
 
 
 find_data()
